166555067ad5f68d6cc41215b2e357a8b0f29b30
Grant Custer <grantcuster@gmail.com>
2019-03-20 16:04:09 -0400

    Updates

---
+++ precache-manifest.b578ac24a9b7e3ae1ff6603c7557c43e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "df02ae951c331dd824c1",
+    "url": "/static/css/main.1d5fbb0a.chunk.css"
+  },
+  {
+    "revision": "df02ae951c331dd824c1",
+    "url": "/static/js/main.df02ae95.chunk.js"
+  },
+  {
+    "revision": "563966fb80cd34d5a217",
+    "url": "/static/js/1.563966fb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0fe304ab4da57c0d18ae803661491d58",
+    "url": "/static/media/IBMPlexMono-Bold.0fe304ab.woff2"
+  },
+  {
+    "revision": "e013d7fb192edf9a162083034de4c4dc",
+    "url": "/static/media/IBMPlexMono-Italic.e013d7fb.woff2"
+  },
+  {
+    "revision": "c7832ed5479c8b9eb5c4733047ecacbd",
+    "url": "/static/media/IBMPlexMono-Regular.c7832ed5.woff2"
+  },
+  {
+    "revision": "4289aa978c126b40c79130726684a5a7",
+    "url": "/static/media/IBMPlexMono-BoldItalic.4289aa97.woff2"
+  },
+  {
+    "revision": "e497c7d84d1cd863756c67d4772e18de",
+    "url": "/static/media/IBMPlexMono-Bold.e497c7d8.woff"
+  },
+  {
+    "revision": "3bbb37eb2e29eed031eaaf63664b21aa",
+    "url": "/static/media/IBMPlexMono-Regular.3bbb37eb.woff"
+  },
+  {
+    "revision": "0cc0d044dbb4bff360916bceb8f288d0",
+    "url": "/static/media/IBMPlexMono-BoldItalic.0cc0d044.woff"
+  },
+  {
+    "revision": "cb143f1453de62a6febc058dfc352f0f",
+    "url": "/static/media/IBMPlexMono-Italic.cb143f14.woff"
+  },
+  {
+    "revision": "e99081e5c038e96111adcf4c67f02545",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/activelearner/precache-manifest.52a54fbdf86c023250ec234cfe4487a8.js"
+  "/precache-manifest.b578ac24a9b7e3ae1ff6603c7557c43e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/activelearner/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
