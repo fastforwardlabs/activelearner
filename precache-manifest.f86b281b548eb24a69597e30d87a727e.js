29f465f73d88bbba89cc888c3aea5b412a411d5f
Grant Custer <grantcuster@gmail.com>
2019-03-29 10:11:31 -0400

    Updates

---
+++ precache-manifest.f86b281b548eb24a69597e30d87a727e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c52885b809670496cd2b",
+    "url": "/static/css/main.84688030.chunk.css"
+  },
+  {
+    "revision": "c52885b809670496cd2b",
+    "url": "/static/js/main.c52885b8.chunk.js"
+  },
+  {
+    "revision": "53e89b278a2017e59612",
+    "url": "/static/js/1.53e89b27.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7832ed5479c8b9eb5c4733047ecacbd",
+    "url": "/static/media/IBMPlexMono-Regular.c7832ed5.woff2"
+  },
+  {
+    "revision": "0fe304ab4da57c0d18ae803661491d58",
+    "url": "/static/media/IBMPlexMono-Bold.0fe304ab.woff2"
+  },
+  {
+    "revision": "e013d7fb192edf9a162083034de4c4dc",
+    "url": "/static/media/IBMPlexMono-Italic.e013d7fb.woff2"
+  },
+  {
+    "revision": "4289aa978c126b40c79130726684a5a7",
+    "url": "/static/media/IBMPlexMono-BoldItalic.4289aa97.woff2"
+  },
+  {
+    "revision": "e497c7d84d1cd863756c67d4772e18de",
+    "url": "/static/media/IBMPlexMono-Bold.e497c7d8.woff"
+  },
+  {
+    "revision": "3bbb37eb2e29eed031eaaf63664b21aa",
+    "url": "/static/media/IBMPlexMono-Regular.3bbb37eb.woff"
+  },
+  {
+    "revision": "cb143f1453de62a6febc058dfc352f0f",
+    "url": "/static/media/IBMPlexMono-Italic.cb143f14.woff"
+  },
+  {
+    "revision": "0cc0d044dbb4bff360916bceb8f288d0",
+    "url": "/static/media/IBMPlexMono-BoldItalic.0cc0d044.woff"
+  },
+  {
+    "revision": "42ff720a9b719f93b2fe698cf46dab2e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67a9376ffa6994e80e09b92bb80349c1.js"
+  "/precache-manifest.f86b281b548eb24a69597e30d87a727e.js"
 );
 
 workbox.clientsClaim();
